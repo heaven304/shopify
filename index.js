baae5263b9639849e9bb77553552fe58119cf412
HeavenKapopara <hevan@shilpmis.com>
2021-03-19 00:07:40 +0530

    init commmit

---
+++ index.js
@@ -0,0 +1,19 @@
+const { default: axios } = require('axios')
+const express = require('express')
+const app = express()
+const port = 4000
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  axios.get("https://b04073ec68aa548224d8ec8ed9040ccd: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@the-roy-in.myshopify.com/admin/api/2021-01/orders.json?status=any").then(res=>{
+      console.log('res',res.data);
+  }  
+  ).catch(err=>{
+      console.error('err',err)
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
