51f04cb4027cc97e735b1a9fbe4ad02b5dd4c180
HeavenKapopara <hevan@shilpmis.com>
2021-03-19 00:49:58 +0530

    change in package json file

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 80
 
-app.get('/', (req, res) => {
+app.get('/orders', (req, res) => {
   res.send('Hello World!')
   axios.get("https://b04073ec68aa548224d8ec8ed9040ccd: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@the-roy-in.myshopify.com/admin/api/2021-01/orders.json?status=any").then(res=>{
       console.log('res',res.data);
+      res.status(200).send(res.data)
   }  
   ).catch(err=>{
       console.error('err',err)
