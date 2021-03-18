72123efdb1250529df19b035f5bb7d4c8f223d7f
HeavenKapopara <hevan@shilpmis.com>
2021-03-19 00:59:31 +0530

    change in package json file

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const port = process.env.PORT || 80
 
 app.get('/orders', (req, res) => {
   // res.send('Hello World!')
-  axios.get("https://b04073ec68aa548224d8ec8ed9040ccd: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@the-roy-in.myshopify.com/admin/api/2021-01/orders.json?status=any").then(res=>{
-      console.log('res',res.data);
-      res.status(200).end(res.data)
+  axios.get("https://b04073ec68aa548224d8ec8ed9040ccd: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@the-roy-in.myshopify.com/admin/api/2021-01/orders.json?status=any").then(result=>{
+      console.log('res',result.data);
+      res.send(result.data)
   }  
   ).catch(err=>{
       console.error('err',err)
